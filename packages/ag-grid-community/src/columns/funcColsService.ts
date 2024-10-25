e964d69db8d614852b0628c057dfe711373efef7
Steph Meslin-Weber <steph@tangency.co.uk>
2024-10-25 15:42:27 +0100

    AG-13079 - move function into columnModel

--- packages/ag-grid-community/src/columns/funcColsService.ts
+++ packages/ag-grid-community/src/columns/funcColsService.ts
@@ -3,23 +3,17 @@ import { BeanStub } from '../context/beanStub';
 import type { BeanCollection } from '../context/context';
 import type { AgColumn } from '../entities/agColumn';
 import type { ColumnEventType } from '../events';
-import type { IAggFuncService } from '../interfaces/iAggFuncService';
 import type { IColsService } from '../interfaces/iColsService';
-import type { ColKey, ColumnModel, Maybe } from './columnModel';
-import type { ColumnState } from './columnStateService';
+import type { ColKey, Maybe } from './columnModel';
 
 export class FuncColsService extends BeanStub implements NamedBean {
     beanName = 'funcColsSvc' as const;
 
-    private colModel: ColumnModel;
-    private aggFuncSvc?: IAggFuncService;
     private rowGroupColsService?: IColsService;
     private pivotColsService?: IColsService;
     private valueColsService?: IColsService;
 
     public wireBeans(beans: BeanCollection): void {
-        this.colModel = beans.colModel;
-        this.aggFuncSvc = beans.aggFuncSvc;
         this.rowGroupColsService = beans.rowGroupColsService;
         this.pivotColsService = beans.pivotColsService;
         this.valueColsService = beans.valueColsService;
@@ -89,27 +83,5 @@ export class FuncColsService extends BeanStub implements NamedBean {
 
     public removeValueColumns(keys: ColKey[], source: ColumnEventType): void {
         this.valueColsService?.removeColumns(keys, source);
-    }
-
-    public extractCols(source: ColumnEventType, oldProvidedCols: AgColumn[] | undefined): void {
-        this.rowGroupColsService?.extractCols(source, oldProvidedCols);
-        this.pivotColsService?.extractCols(source, oldProvidedCols);
-        this.valueColsService?.extractCols(source, oldProvidedCols);
-    }
-
-    public generateColumnStateForRowGroupAndPivotIndexes(
-        updatedRowGroupColumnState: { [colId: string]: ColumnState },
-        updatedPivotColumnState: { [colId: string]: ColumnState }
-    ): ColumnState[] {
-        // Generally columns should appear in the order they were before. For any new columns, these should appear in the original col def order.
-        // The exception is for columns that were added via `addGroupColumns`. These should appear at the end.
-        // We don't have to worry about full updates, as in this case the arrays are correct, and they won't appear in the updated lists.
-
-        const existingColumnStateUpdates: { [colId: string]: ColumnState } = {};
-
-        this.rowGroupColsService?.orderColumns(existingColumnStateUpdates, updatedRowGroupColumnState);
-        this.pivotColsService?.orderColumns(existingColumnStateUpdates, updatedPivotColumnState);
-
-        return Object.values(existingColumnStateUpdates);
     }
 }
