fb894f91b40754f3f692240faadefa6102b0d6ab
Stephen Cooper <scooperdev@gmail.com>
2024-11-08 14:23:57 +0000

    Add a missing export

--- packages/ag-grid-community/src/main.ts
+++ packages/ag-grid-community/src/main.ts
@@ -847,6 +847,7 @@ export {
     TabToNextCellParams,
     GetContextMenuItemsParams,
     GetMainMenuItemsParams,
+    GetChartMenuItemsParams,
     PostProcessPopupParams,
     IsExternalFilterPresentParams,
     InitialGroupOrderComparatorParams,
