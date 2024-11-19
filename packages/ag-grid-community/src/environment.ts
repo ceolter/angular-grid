b0e9bb53e6739463303fa922fb29976917e3def9
Bernie Sumption <bernie@berniecode.com>
2024-11-19 13:11:46 +0000

    AG-13381 fix alignment of cell text when rows have thick borders

--- packages/ag-grid-community/src/environment.ts
+++ packages/ag-grid-community/src/environment.ts
@@ -1,10 +1,9 @@
-import { themeQuartz } from 'ag-grid-community';
-
 import type { NamedBean } from './context/bean';
 import { BeanStub } from './context/beanStub';
 import type { BeanCollection } from './context/context';
 import { ThemeImpl } from './theming/Theme';
 import { IS_SSR, _injectCoreAndModuleCSS, _injectGlobalCSS } from './theming/inject';
+import { themeQuartz } from './theming/parts/theme/themes';
 import { _observeResize } from './utils/dom';
 import { _error, _warn } from './validation/logging';
 
@@ -22,6 +21,12 @@ const LIST_ITEM_HEIGHT: Variable = {
     cssName: '--ag-list-item-height',
     changeKey: 'listItemHeightChanged',
     defaultValue: 24,
+};
+const ROW_BORDER_WIDTH: Variable = {
+    cssName: '--ag-row-border',
+    changeKey: 'rowBorderWidthChanged',
+    defaultValue: 1,
+    border: true,
 };
 
 let paramsId = 0;
@@ -53,6 +58,8 @@ export class Environment extends BeanStub implements NamedBean {
         this.getSizeEl(ROW_HEIGHT);
         this.getSizeEl(HEADER_HEIGHT);
         this.getSizeEl(LIST_ITEM_HEIGHT);
+        this.getSizeEl(ROW_BORDER_WIDTH);
+        this.refreshRowBorderWidthVariable();
     }
 
     public getDefaultRowHeight(): number {
@@ -72,8 +79,12 @@ export class Environment extends BeanStub implements NamedBean {
         return Math.min(36, this.getDefaultRowHeight());
     }
 
-    public getDefaultListItemHeight() {
+    public getDefaultListItemHeight(): number {
         return this.getCSSVariablePixelValue(LIST_ITEM_HEIGHT);
+    }
+
+    public getRowBorderWidth(): number {
+        return this.getCSSVariablePixelValue(ROW_BORDER_WIDTH);
     }
 
     public applyThemeClasses(el: HTMLElement) {
@@ -173,7 +184,16 @@ export class Environment extends BeanStub implements NamedBean {
         }
 
         sizeEl = document.createElement('div');
-        sizeEl.style.width = `var(${variable.cssName}, ${NO_VALUE_SENTINEL}px)`;
+        const { border } = variable;
+        if (border) {
+            sizeEl.className = 'ag-measurement-element-border';
+            sizeEl.style.setProperty(
+                '--ag-measurement-border',
+                `var(${variable.cssName}, solid ${NO_VALUE_SENTINEL}px`
+            );
+        } else {
+            sizeEl.style.width = `var(${variable.cssName}, ${NO_VALUE_SENTINEL}px)`;
+        }
         container.appendChild(sizeEl);
         this.sizeEls.set(variable, sizeEl);
 
@@ -201,10 +221,18 @@ export class Environment extends BeanStub implements NamedBean {
     }
 
     private fireGridStylesChangedEvent(change: ChangeKey): void {
+        if (change === 'rowBorderWidthChanged') {
+            this.refreshRowBorderWidthVariable();
+        }
         this.eventSvc.dispatchEvent({
             type: 'gridStylesChanged',
             [change]: true,
         });
+    }
+
+    private refreshRowBorderWidthVariable(): void {
+        const width = this.getCSSVariablePixelValue(ROW_BORDER_WIDTH);
+        this.eGridDiv.style.setProperty('--ag-internal-row-border-width', `${width}px`);
     }
 
     private handleThemeGridOptionChange(): void {
@@ -262,8 +290,14 @@ type Variable = {
     cssName: string;
     changeKey: ChangeKey;
     defaultValue: number;
+    border?: boolean;
 };
 
-type ChangeKey = 'themeChanged' | 'headerHeightChanged' | 'rowHeightChanged' | 'listItemHeightChanged';
+type ChangeKey =
+    | 'themeChanged'
+    | 'headerHeightChanged'
+    | 'rowHeightChanged'
+    | 'listItemHeightChanged'
+    | 'rowBorderWidthChanged';
 
 const NO_VALUE_SENTINEL = 15538;
