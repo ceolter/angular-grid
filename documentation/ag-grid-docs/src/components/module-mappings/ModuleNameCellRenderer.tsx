bf3a01dad61a8da2661f9064d154625cba2b2657
Mark Durrant <mark.j.durrant@gmail.com>
2024-11-26 10:12:24 +0000

    AG-13468 - Tweak modules name styling

--- documentation/ag-grid-docs/src/components/module-mappings/ModuleNameCellRenderer.tsx
+++ documentation/ag-grid-docs/src/components/module-mappings/ModuleNameCellRenderer.tsx
@@ -1,13 +1,9 @@
-import { Icon } from '@ag-website-shared/components/icon/Icon';
-import { getFrameworkFromPath } from '@components/docs/utils/urlPaths';
-import { urlWithPrefix } from '@utils/urlWithPrefix';
-
 import type { CustomCellRendererProps } from 'ag-grid-react';
 
-import styles from './ModuleCellRenderer.module.scss';
+import styles from './ModuleNameCellRenderer.module.scss';
 
 export function ModuleNameCellRenderer({ data }: CustomCellRendererProps) {
     const moduleName = data.moduleName;
 
-    return moduleName ? <code>{moduleName}</code> : <span></span>;
+    return moduleName ? <code className={styles.moduleName}>{moduleName}</code> : null;
 }
