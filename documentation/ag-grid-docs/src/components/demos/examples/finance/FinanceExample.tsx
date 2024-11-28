c68db77f7df08ec6104370cc7b13f08f84a3a2d1
Tak Tran <contact@tutaktran.com>
2024-11-28 14:49:35 +0000

    AG-13353 - Don't allow timeline to go to 0

--- documentation/ag-grid-docs/src/components/demos/examples/finance/FinanceExample.tsx
+++ documentation/ag-grid-docs/src/components/demos/examples/finance/FinanceExample.tsx
@@ -96,7 +96,11 @@ export const FinanceExample: React.FC<Props> = ({
                     }
                     const rnd = (Math.random() * PERCENTAGE_CHANGE) / 100;
                     const change = Math.random() > 0.5 ? 1 - rnd : 1 + rnd;
-                    const price = item.price * change;
+                    const price =
+                        item.price < 10
+                            ? item.price * change
+                            : // Increase price if it is too low, so it does not hang around 0
+                              Math.random() * 40 + 10;
 
                     const timeline = item.timeline.slice(1, item.timeline.length).concat(Number(price.toFixed(2)));
 
