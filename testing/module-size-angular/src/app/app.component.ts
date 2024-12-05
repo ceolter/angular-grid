30bc4751ac0031e508d08375a4b7818e00daf060
Stephen Cooper <scooperdev@gmail.com>
2024-12-05 11:24:06 +0000

    add comment

--- testing/module-size-angular/src/app/app.component.ts
+++ testing/module-size-angular/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { GridWrapperComponent } from './grid-wrapper/grid-wrapper.component';
     standalone: true,
     imports: [GridWrapperComponent],
     styleUrls: ['./app.component.css'],
-    // links to the grid-wrapper component
+    // Wrap the grid-wrapper component in a deferred component so that it gets a separate chunk which does not include Angular in its size
     template: `
         <h1>Grid Wrapper</h1>
         @defer {
