efc9e5b1c5375ca9a23c4e7748f0a651f0cb79af
Mark Durrant <mark.j.durrant@gmail.com>
2024-12-12 11:15:46 +0000

    AG-13685 - Fix multiline line-height issues

--- documentation/ag-grid-docs/src/components/changelog/Changelog.tsx
+++ documentation/ag-grid-docs/src/components/changelog/Changelog.tsx
@@ -274,6 +274,7 @@ export const Changelog = () => {
                 filter: 'agTextColumnFilter',
                 wrapText: true,
                 autoHeight: true,
+                cellStyle: { 'line-height': 24, 'padding-top': 8, 'padding-bottom': 8 },
             },
             {
                 field: 'version',
