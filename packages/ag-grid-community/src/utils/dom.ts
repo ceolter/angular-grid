9bc02de4d263e83ee6a9a6c41311c3b649f51bed
Elias Malik <elias.malik@ag-grid.com>
2024-12-18 16:30:41 +0000

    comment

--- packages/ag-grid-community/src/utils/dom.ts
+++ packages/ag-grid-community/src/utils/dom.ts
@@ -532,6 +532,10 @@ export function _getTextSelectionRanges(beans: BeanCollection): { selection: Sel
     return { selection, ranges };
 }
 
+/**
+ * FF and Safari remove text selections when the focus changes. This is inconsistent with Chrome, whose behaviour
+ * we prefer in this case. This utility preserves whatever text selection exists before the given action is taken.
+ */
 export function _preserveRangesWhile(beans: BeanCollection, fn: () => void): void {
     if (!_isBrowserFirefox() && !_isBrowserSafari()) {
         return fn();
