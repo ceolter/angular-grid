443d298d594658d5a2365a2f6900e735b3acc07f
seanlandsman <sean@thelandsmans.com>
2025-01-13 14:58:19 +0000

    Add styles check to validation script

--- scripts/deployments/validateDistFolders.sh
+++ scripts/deployments/validateDistFolders.sh
@@ -109,6 +109,15 @@ validatePackages()
         echo "ERROR: $current_dist should have $expected_umd umd files"
         exit 1
       fi
+
+      expected_styles=35
+      current_dist=$current_root_dir/styles
+      count=`find $current_dist | wc -l | tr -d ' '`
+      if [[ $count -ne $expected_styles ]]
+      then
+        echo "ERROR: $current_dist should have $expected_styles style files"
+        exit 1
+      fi
     elif [[ ${frameworks[@]} =~ $directory ]]
     then
       # a framework - here we're just checking there are files in the package as a sanity check
@@ -157,10 +166,10 @@ validateVue3()
 }
 
 # check all expected modules & packages are there
-validateExpectedDirs "dist/artifacts/contents/community-modules" 3
+validateExpectedDirs "dist/artifacts/contents/community-modules" 2
 validateExpectedDirs "dist/artifacts/contents/packages" 6
 
-validateExpectedDirs "dist/artifacts/community-modules" 3
+validateExpectedDirs "dist/artifacts/community-modules" 2
 validateExpectedDirs "dist/artifacts/packages" 6
 
 validateModules "dist/artifacts/contents/community-modules"
