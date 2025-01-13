a12643634a76ba5e57b7e6b5738a5422f2d2823f
Kyler Phillips <kylerphillips@hotmail.co.uk>
2025-01-13 09:21:46 +0000

    AG-13591 Update alert to use current framework

--- documentation/ag-grid-docs/src/components/module-callout/components/ModuleCallout.tsx
+++ documentation/ag-grid-docs/src/components/module-callout/components/ModuleCallout.tsx
@@ -1,11 +1,25 @@
 import { Alert } from '@ag-website-shared/components/alert/Alert';
+import { getFrameworkFromPath } from '@components/docs/utils/urlPaths';
+import { urlWithPrefix } from '@utils/urlWithPrefix';
 
 export const ModuleCallout = () => {
+    const framework = getFrameworkFromPath(window.location.pathname);
     return (
         <>
-            <Alert type="module">
+            <Alert type="info">
                 <p>
-                    To use this feature you will need to import the <code>PinnedRowModule</code>.
+                    If you are selecting modules, you will need to import <code>PinnedRowModule</code>. More information
+                    in{' '}
+                    <a
+                        href={urlWithPrefix({
+                            framework,
+                            url: `./modules`,
+                        })}
+                        target={'_blank'}
+                    >
+                        Modules
+                    </a>
+                    .
                 </p>
             </Alert>
         </>
